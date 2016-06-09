19cbdb239193f4c20cd0676f50a9f7a3e39bcf70
Gela <gela_who@yahoo.com>
2016-06-09 19:54:23 +0800

    added links

--- js/jquery.js
+++ js/jquery.js
@@ -3,24 +3,24 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
 
     waffleScroll();
-    
+
     $('.icecream').css({
       'transform' : 'translate(0px, '+ wScroll /10 +'%)'
-    })  
+    })
     $('.logo').css({
       'transform' : 'translate(0px, '+ wScroll /20 +'%)'
     });
-      
+
     $('.rectangle-1').css({
       'transform' : 'translate(0px, -'+ wScroll /30 +'%)'
     });
-    
-    
+
     if(wScroll == $('.about').offset().top){
 
     console.log("hi");
   }
 });
+
 
 $('.description').hover(function(){
     $('.desc-overlay').fadeIn();
