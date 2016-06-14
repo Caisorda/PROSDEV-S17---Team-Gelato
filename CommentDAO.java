35d904d2ff4146c1379af05c0a06e8741d48b1a3
angeline-tan <angeline_jt@yahoo.com.ph>
2016-06-14 18:16:26 +0800

    added comment DAO

--- CommentDAO.java
+++ CommentDAO.java
@@ -7,21 +7,54 @@ import java.util.Iterator;
 
 public class CommentDAO{
     public Iterator getComments(int postid){
-        ArrayList<Comment> comments = new ArrayList();
-        //to do
-        
-        return comments.iterator();
+        conn = (Connection) DBConnection.getConnection();
+        String q = "SELECT * FROM COMMENTS";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		rs = stmt.executeQuery();
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
     }
     
     public void addComment(Comment comment){
-        //to do
+    	conn = (Connection) DBConnection.getConnection();
+        String q = "INSERT INTO COMMENTS(details,date,postId) Values(?,?,?)";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		stmt.setString(1,comment.getDetails());
+		stmt.setString(2,comment.getDate());
+		stmt.setString(3,comment.getPostId());
+		rs = stmt.executeQuery();
+		
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
     }
     
     public void editComment(Comment comment){
-        //to do
+    	conn = (Connection) DBConnection.getConnection();
+        String q = "UPDATE COMMENTS SET details = ?, date = ?, postId = ? WHERE id =?";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		stmt.setString(1,comment.getDetails());
+		stmt.setString(2,comment.getDate());
+		stmt.setString(3,comment.getPostId());
+		stmt.setString(4, comment.getId());
+		rs = stmt.executeQuery();
+		
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
+        
     }
     
     public void deleteComment(int commentid){
-        //to do
+        conn = (Connection) DBConnection.getConnection();
+        String q = "DELETE FROM COMMENTS WHERE id =?";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		stmt.setString(1, comment.getId());
+		rs = stmt.executeQuery();
+		
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
     }
 }
