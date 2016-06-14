efcc618b4e0210e8955eef22ddba5e11f7fe74cb
angeline-tan <angeline_jt@yahoo.com.ph>
2016-06-14 18:24:10 +0800

    updated post DAO

--- PostDAO.java
+++ PostDAO.java
@@ -8,20 +8,60 @@ import java.util.Iterator;
 public class PostDAO{
     public Iterator getPosts(int postid){
         ArrayList<Post> posts = new ArrayList();
-        //to do
-        
+        //to do title,author,description,date
+        conn = (Connection) DBConnection.getConnection();
+        String q = "SELECT * FROM POSTS";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		rs = stmt.executeQuery();
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
         return comments.iterator();
     }
     
     public void addPost(Post post){
         //to do
+		conn = (Connection) DBConnection.getConnection();
+        String q = "INSERT INTO POSTS(title,author,description,date) Values(?,?,?,?)";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		stmt.setString(1,post.getTitle());
+		stmt.setString(2,post.getAuthor());
+		stmt.setString(3,post.getDescription());
+		stmt.setString(4,post.getDate());
+		rs = stmt.executeQuery();
+		
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
     }
     
     public void editPost(Post post){
         //to do
+		conn = (Connection) DBConnection.getConnection();
+        String q = "UPDATE POSTS SET title = ? , author = ?, description = ?, date = ? WHERE id =?";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		stmt.setString(1,post.getTitle());
+		stmt.setString(2,post.getAuthor());
+		stmt.setString(3,post.getDescription());
+		stmt.setString(4,post.getDate());
+		stmt.setString(5,post.getId());
+		rs = stmt.executeQuery();
+		
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
     }
     
     public void deletePost(int postid){
         //to do
+		conn = (Connection) DBConnection.getConnection();
+        String q = "DELETE FROM POSTS WHERE id =?";
+		PreparedStatement stmt;
+		stmt = (PreparedStatement) conn.prepareStatement(q);
+		stmt.setString(1, post.getId());
+		rs = stmt.executeQuery();
+		
+        ResultSetIterator rt = new ResultSetIterator(rs);
+        return rt;
     }
 }
