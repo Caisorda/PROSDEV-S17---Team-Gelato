584c35b2cbfb8e88acdf429c9ec6d9667b724337
Caisorda <juan_salceda@dlsu.edu.ph>
2016-06-16 19:15:23 +0800

    Cleaned DAOs

--- src/PostDAO.java
+++ src/PostDAO.java
@@ -9,59 +9,72 @@ public class PostDAO{
     public Iterator getPosts(int postid){
         ArrayList<Post> posts = new ArrayList();
         //to do title,author,description,date
-        conn = (Connection) DBConnection.getConnection();
+        Connection conn = (Connection) DBConnection.getConnection();
         String q = "SELECT * FROM POSTS";
 		PreparedStatement stmt;
-		stmt = (PreparedStatement) conn.prepareStatement(q);
-		rs = stmt.executeQuery();
-        ResultSetIterator rt = new ResultSetIterator(rs);
-        return rt;
-        return comments.iterator();
+		try {
+			stmt = conn.prepareStatement(q);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()){
+				posts.add(new Post(rs.getInt("id"), rs.getString("title"), rs.getString("author"), rs.getString("description"), null));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//        ResultSetIterator rt = new ResultSetIterator(rs);
+        return posts.iterator();
     }
     
     public void addPost(Post post){
         //to do
-		conn = (Connection) DBConnection.getConnection();
+		Connection conn = (Connection) DBConnection.getConnection();
         String q = "INSERT INTO POSTS(title,author,description,date) Values(?,?,?,?)";
 		PreparedStatement stmt;
-		stmt = (PreparedStatement) conn.prepareStatement(q);
-		stmt.setString(1,post.getTitle());
-		stmt.setString(2,post.getAuthor());
-		stmt.setString(3,post.getDescription());
-		stmt.setString(4,post.getDate());
-		rs = stmt.executeQuery();
-		
-        ResultSetIterator rt = new ResultSetIterator(rs);
-        return rt;
+		try {
+			stmt = conn.prepareStatement(q);
+			stmt.setString(1,post.getTitle());
+			stmt.setString(2,post.getAuthor());
+			stmt.setString(3,post.getDescription());
+			stmt.setString(4,post.getDate());
+			stmt.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     public void editPost(Post post){
         //to do
-		conn = (Connection) DBConnection.getConnection();
+		Connection conn = (Connection) DBConnection.getConnection();
         String q = "UPDATE POSTS SET title = ? , author = ?, description = ?, date = ? WHERE id =?";
 		PreparedStatement stmt;
-		stmt = (PreparedStatement) conn.prepareStatement(q);
-		stmt.setString(1,post.getTitle());
-		stmt.setString(2,post.getAuthor());
-		stmt.setString(3,post.getDescription());
-		stmt.setString(4,post.getDate());
-		stmt.setString(5,post.getId());
-		rs = stmt.executeQuery();
-		
-        ResultSetIterator rt = new ResultSetIterator(rs);
-        return rt;
+		try {
+			stmt = conn.prepareStatement(q);
+			stmt.setString(1,post.getTitle());
+			stmt.setString(2,post.getAuthor());
+			stmt.setString(3,post.getDescription());
+			stmt.setString(4,post.getDate());
+			stmt.setInt(5,post.getId());
+			stmt.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     public void deletePost(int postid){
         //to do
-		conn = (Connection) DBConnection.getConnection();
+		Connection conn = (Connection) DBConnection.getConnection();
         String q = "DELETE FROM POSTS WHERE id =?";
 		PreparedStatement stmt;
-		stmt = (PreparedStatement) conn.prepareStatement(q);
-		stmt.setString(1, post.getId());
-		rs = stmt.executeQuery();
-		
-        ResultSetIterator rt = new ResultSetIterator(rs);
-        return rt;
+		try {
+			stmt = conn.prepareStatement(q);
+			stmt.setInt(1, postid);
+			stmt.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
